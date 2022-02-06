450d65b1e308cd26b636eb60952a85833dda7f02
Spencer Lee Meredith <16077691+SLM-Dev@users.noreply.github.com>
2022-02-06 01:46:20 +0000

    🚧 WIP TacoCat Project 🚧WIP Logo 🚧WIP Code Explantion Prism 🚧🚀WIP Host After Finish

--- js/site.js
+++ js/site.js
@@ -1,1 +1,59 @@
-// get o
+        // get our user input with a function
+        function getValue() {
+
+    //make sure the alert is invisible
+    document.getElementById("alert").classList.add("invisible");       
+    
+    // get user string for the page
+    let  userString = document.getElementById("userString").value;
+    
+        // check for a palindrome 
+        let returnObj = checkPalindrome(userString);
+    
+        //display out message to the screen as a function
+       displayMessage(returnObj); 
+    
+       }
+       //check if string is a palindrome
+    function checkPalindrome(userString) {
+
+      // taco cat covert all character to lower case
+        userString = userString.toLowerCase();
+
+        //remove spaces and special characters
+         let regex = /[^a-z0-9]/gi;
+        userString = userString.replace(regex,"");
+
+        let revString = [];
+        let returnObj = {};
+        
+        
+        // reverse the string
+        for (let index = userString.length - 1; index >= 0; index--) {
+            revString += userString[index];
+        }        
+         if(revString == userString) {
+            returnObj.isPalindrome = true;
+            returnObj.msg = "Well done! You entered a palindrome!";
+        }   
+        else {
+            returnObj.isPalindrome = false;
+            returnObj.msg = "Sorry, you entered a non-palindrome!";
+        }
+        returnObj.reversed = revString;
+
+        return returnObj;
+
+         }
+
+
+        //display a message to the string
+        function displayMessage(returnObj) {
+         
+            document.getElementById("alertHeader").innerHTML = returnObj.msg;
+            document.getElementById("msg").innerHTML = `Your phrase reversed is: ${returnObj.reversed}`;
+            document.getElementById("alert").classList.remove("invisible");
+            
+        }
+        
+        
